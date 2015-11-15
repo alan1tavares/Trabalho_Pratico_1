3ad68625e41744ec96d4f41ff09df9a248bcfe81
Alan Tavares <alan1tavares@gmail.com>
2015-11-15 18:05:06 -0300

    Criado a classes dos agentes. Configurado o simulador para aceitar os agentes

---
+++ AI/ai/worlds/vacuum/TiagoAlanAgent2AgentVacuum.java
@@ -0,0 +1,17 @@
+package ai.worlds.vacuum;
+
+import ai.worlds.Agent;
+import ai.worlds.Environment;
+
+public class TiagoAlanAgent2AgentVacuum extends VacuumAgent {
+	/**
+	 * Agente baseado em modelos
+	 */
+	@Override
+	public void determineAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ AI/ai/worlds/vacuum/TiagoAlanAgente1VacuumAgent.java
@@ -0,0 +1,13 @@
+package ai.worlds.vacuum;
+
+public class TiagoAlanAgente1VacuumAgent extends VacuumAgent {
+	/**
+	 * Agente reativo simples
+	 */
+	@Override
+	public void determineAction() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
